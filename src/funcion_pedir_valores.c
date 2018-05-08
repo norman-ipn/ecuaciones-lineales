b63e3a9e8b0d2826ec5b5d98231718f0082e8423
sabrinapens <pensamiento.sabrina@gmail.com>
2018-05-08 12:57:00 -0500

    .

--- src/funcion_pedir_valores.c
+++ src/funcion_pedir_valores.c
@@ -1,27 +1,39 @@
 #include <stdio.h>
 
+int main(void){ 
 
-int
-main (void)
-{
-  float matriz[4][5];
-  int fil = 0;
-  int col = 0;
+float matriz [4][5]; /*El tamaño de la matriz ya la definen en otra función así que aquí usaré una matriz ya definida */
 
-  for (fil = 0; fil < 4; fil++) {
-      for (col = 0; col < 5; col++){
-	  printf ("Ingrese el coeficiente del lugar matriz[%d][%d]/t", fil + 1, col + 1);	//j+1 e i+1 porque no se puede empezar a contar desde una fila que no existe (pues ambas variables están inicializadas en cero)
-	  scanf ("%f", &matriz[fil][col]);	//Hay que guardar estas cosas en el lugar que les corresponde
+int fil=0;
+
+int col=0;
+
+
+
+for (fil=0; fil<4; fil++){ /* necesitamos iteraciones para ir pidiendo los coeficientes y acomodarlos*/
+
+	for(col=0; col<5; col++){
+
+		printf("Ingrese el coeficiente del lugar matriz[%d][%d]\t", fil+1, col+1); //j+1 e i+1 porque no se puede empezar a contar desde una fila que no existe (pues ambas variables están inicializadas en cero)
+
+		scanf("%f",&matriz[fil][col]); //Hay que guardar estas cosas en el lugar que les corresponde
+
 	}
 
-      for (fil = 0; fil < 4; fil++){
-	  for (col = 0; col < 5; col++){
-	      printf ("%.2f\t", matriz[fil][col]);	//Imprimimos nada más para comprobar que la matriz se haya llenado correctamente
-	    }
-	  printf ("\n");
+}
+
+for (fil=0; fil<4; fil++){
+
+	for (col=0; col<5; col++){
+
+		printf("%.2f\t", matriz[fil][col]); //Imprimimos nada más para saber si sirve :'v
+
 	}
 
-    }
+	printf("\n");
 
-  return 0;
+}
+
+return 0;
+
 }
