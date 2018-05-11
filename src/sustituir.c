f6d25343316cc9ccc0674c7ab344bb45b37f54d9
HugoAndres <huggo.ipn.1997@gmail.com>
2018-05-10 22:56:06 -0500

    Corrección de la función sustituir

--- src/sustituir.c
+++ src/sustituir.c
@@ -1,7 +1,8 @@
 float
 sustituir (int n)
 {
-  int i = 0, j = 0, sum = 0, A = 0;
+  int i = 0, j = 0;
+  float A[n][n], sum = 0.0, x[n];
   x[n] = A[n][n + 1] / A[n][n];
   for (i = n - 1; i >= 1; i--)
     {
