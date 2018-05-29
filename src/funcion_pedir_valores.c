b4a99188c723a0f4ec0c6e5f69ed125e7bda8355
JACM8544 <jorarconmer@gmail.com>
2018-05-29 11:31:53 -0500

    cambie funcion pedir valores a terminos de A

--- src/funcion_pedir_valores.c
+++ src/funcion_pedir_valores.c
@@ -2,8 +2,8 @@
 
 int pedirvalores(void){ 
 
-float A, B;
-float matriz [A][B];
+float A;
+float matriz [A][A+1];
 
 int fil=0;
 
@@ -13,7 +13,7 @@ int col=0;
 
 for (fil=0; fil<A; fil++){
 
-	for(col=0; col<B; col++){
+	for(col=0; col<(A+1); col++){
 
 		printf("Ingrese el coeficiente del lugar matriz[%d][%d]\t", fil+1, col+1);
 
