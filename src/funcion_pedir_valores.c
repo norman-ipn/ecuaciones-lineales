91f1f1f151fc2ec2ed38afd7b10e485513b95a9b
Alejandro Ramirez Salazar <alejandro.ramirezs117@gmail.com>
2018-05-29 22:20:58 -0500

    Corregí funcion_pedir_valores.c y agregué funcion_pedir_valores.o

--- src/funcion_pedir_valores.c
+++ src/funcion_pedir_valores.c
@@ -1,28 +1,14 @@
 #include <stdio.h>
-
 int pedirvalores(void){ 
-
-int A;
-float matriz [A][A+1];
-
-int fil=0;
-
-int col=0;
-
-
-
+ int A;
+ float matriz [A][A+1];
+ int fil=0;
+ int col=0;
 for (fil=0; fil<A; fil++){
-
 	for(col=0; col<(A+1); col++){
-
 		printf("Ingrese el coeficiente del lugar matriz[%d][%d]\t", fil+1, col+1);
-
-		scanf("%f",&matriz[fil][col]);}}
-
-	}
-
-}
-
+		scanf("%f",&matriz[fil][col]);
+				    }
+			 }		      
 return 0;
-
 }
