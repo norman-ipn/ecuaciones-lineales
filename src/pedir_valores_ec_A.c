46c954f9354100d4bacb9a4e548dab0bdc055c56
zorox69 <michelangel.alex@gmail.com>
2018-06-03 18:27:24 -0500

    indento y quito el main

--- src/pedir_valores_ec_A.c
+++ src/pedir_valores_ec_A.c
@@ -3,20 +3,17 @@
 /**Tomar los datos de la ecuacion A**/
 /************************************/
 #include<stdio.h>
-int main()
 {
-    int a,b,i;
-    int c[i];
-	{
-	   printf("valor de i: "); /*numero de variables*/
-	   scanf("%d",&i);
- 	  for(b=0;b<i;b++);
-    		{
-		  printf("ingrese un valor: \n");
-		  scanf("%d",&c[b]);
-		  printf("tu valor %d es: %d\n",b,c[b]);
-		}
-	}
-    
-    return(0);
+  int a, b, i;
+  int c[i];
+  {
+    printf ("valor de i: ");	/*numero de variables, se tiene que quitar cuando se compile con el resto*/
+    scanf ("%d", &i);
+    for (b = 0; b < i; b++);
+    {
+      printf ("ingrese un valor: \n");
+      scanf ("%d", &c[b]);
+    }
+  }
+   return (0);
 }
