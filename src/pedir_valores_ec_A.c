fc706de6339996267a84dd093429fc19f36e76cb
zorox69 <michelangel.alex@gmail.com>
2018-06-03 18:05:57 -0500

    subo la parte para pedir los valores de la ecuacion A

---
+++ src/pedir_valores_ec_A.c
@@ -0,0 +1,22 @@
+/************************************/
+/***Caamal Briseño Diego Alejandro***/
+/**Tomar los datos de la ecuacion A**/
+/************************************/
+#include<stdio.h>
+int main()
+{
+    int a,b,i;
+    int c[i];
+	{
+	   printf("valor de i: "); /*numero de variables*/
+	   scanf("%d",&i);
+ 	  for(b=0;b<i;b++);
+    		{
+		  printf("ingrese un valor: \n");
+		  scanf("%d",&c[b]);
+		  printf("tu valor %d es: %d\n",b,c[b]);
+		}
+	}
+    
+    return(0);
+}
