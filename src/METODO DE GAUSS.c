67796b63359eea3bee4f5fd9b8e294b18d07a5dc
shunashyarellano-ipn <shunashyn.arellano@gmail.com>
2018-06-04 13:00:22 -0500

    Ya está indentado

--- src/METODO DE GAUSS.c
+++ src/METODO DE GAUSS.c
@@ -1,36 +1,39 @@
 #include<stdio.h>
 #define TAM 100
- 
- //Pedir N
- 
-int datos (void);
 
+  //Pedir N
+int datos (void);
 {
-    int i, j;
-    int n;
-    n = datos();
- 
-    float MAT[TAM][TAM];
-   
-    for (i = 0; i < n; i++){
-        for (j = 0; j < n; j++){
-            MAT[i][j] = 0; //Asigna valor a la matriz
-        }
+  int i, j;
+  int n;
+  n = datos ();
+  float MAT[TAM][TAM];
+  for (i = 0; i < n; i++)
+    {
+      for (j = 0; j < n; j++)
+	{
+	  MAT[i][j] = 0;	//Asigna valor a la matriz
+	}
     }
- 
-    return 0;
+  return 0;
 }
- 
-int datos (){ /* funcion para que pida tamaño de la matriz */
-    int i;
-    do{
-        printf("Introduzca el numero de variables que incluira la matriz (Introduzca un numero menor a 100):\n");
-        scanf("%i", &i);    /*introducimos el tamaño de la matriz*/
-        if(i > TAM && i <= 0){
-            printf("el tamaño no es valido, intente de nuevo.");
-        }else
-            break;
-    }while(1);  /*Si no es del tamaño adecuado, indicarlo*/
- 
-    return i;
+
+int
+datos ()
+{				/* funcion para que pida tamaño de la matriz */
+  int i;
+  do
+    {
+      printf
+	("Introduzca el numero de variables que incluira la matriz (Introduzca un numero menor a 100):\n");
+      scanf ("%i", &i);	/*introducimos el tamaño de la matriz */
+      if (i > TAM && i <= 0)
+	{
+	  printf ("el tamaño no es valido, intente de nuevo.");
+	}
+      else
+	break;
+    }
+  while (1);			/*Si no es del tamaño adecuado, indicarlo */
+  return i;
 }
