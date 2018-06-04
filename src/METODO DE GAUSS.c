2f52ebed3095543d6e88c56e77f235f3efa68451
danikrkic <danikrkic@gmail.com>
2018-06-04 13:26:05 -0500

    modifique la funcion 'obtener datos' del codigo

--- src/METODO DE GAUSS.c
+++ src/METODO DE GAUSS.c
@@ -2,16 +2,18 @@
 #define TAM 100
  
  //Pedir N
+ //prototipo
  
 int datos (void);
- 
-main(void)
+
+ //Aqui iba el main
+int main(void)
 {
     int i, j;
     int n;
     n = datos();
  
-    float MAT[TAM][TAM];
+    int MAT[TAM][TAM];
    
     for (i = 0; i < n; i++){
         for (j = 0; j < n; j++){
@@ -23,15 +25,14 @@ main(void)
 }
  
 int datos (){ /* funcion para que pida tamaño de la matriz */
-    int i;
-    do{
-        printf("Introduzca el numero de variables que incluira la matriz (Introduzca un numero menor a 100):\n");
-        scanf("%i", &i);    /*introducimos el tamaño de la matriz*/
-        if(i > TAM && i <= 0){
-            printf("el tamaño no es valido, intente de nuevo.");
-        }else
-            break;
-    }while(1);  /*Si no es del tamaño adecuado, indicarlo*/
- 
-    return i;
+   int i;
+   int f;
+   puts("Introduzca el numero de variables que incluira la matriz (Introduzca un numero menor a 100):");
+   scanf("%i", &i);    /*introducimos el tamaño de la matriz*/
+        while(i > TAM || i <= 0){
+            printf("el tamaño no es valido, intente de nuevo. Introduzca el valor de nuevo");
+	 scanf("%i", &i);
+	}
+f=i;	
+    return f;
 }
