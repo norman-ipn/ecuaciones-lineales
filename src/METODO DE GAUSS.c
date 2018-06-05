af947f9a7a374da9327757038f9e461c8719e96b
danikrkic <danikrkic@gmail.com>
2018-06-04 20:36:29 -0500

    corregi mi error al fusionar archivos e indente

--- src/METODO DE GAUSS.c
+++ src/METODO DE GAUSS.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
 #define TAM 100
 
-  //Pedir N
-int datos (void);
+ //Pedir N
+ //prototipo
+int datos ();
+
+int
+main ()
 {
   int i, j;
   int n;
   n = datos ();
-  float MAT[TAM][TAM];
+  int MAT[TAM][TAM];
   for (i = 0; i < n; i++)
     {
       for (j = 0; j < n; j++)
@@ -22,18 +26,14 @@ int
 datos ()
 {				/* funcion para que pida tamaño de la matriz */
   int i;
-  do
+  int f;
+  printf
+    ("Introduzca el numero de variables que incluira la matriz (Introduzca un numero menor a 100):\n");
+  scanf ("%i", &i);		/*introducimos el tamaño de la matriz */
+  while (i > TAM && i > 0)
     {
-      printf
-	("Introduzca el numero de variables que incluira la matriz (Introduzca un numero menor a 100):\n");
-      scanf ("%i", &i);		/*introducimos el tamaño de la matriz */
-      if (i > TAM && i <= 0)
-	{
-	  printf ("el tamaño no es valido, intente de nuevo.");
-	}
-      else
-	break;
+      printf ("El valor excede a 100, introduzca uno menor\n");
+      scanf ("%i", &i);
     }
-  while (1);			/*Si no es del tamaño adecuado, indicarlo */
   return i;
 }
