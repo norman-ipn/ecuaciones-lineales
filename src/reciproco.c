8f052481e19935a2e249f5018eb3aafd53370d85
Mayer Abraham Perez Gonzalez <mayer_abraham@hotmail.com>
2018-06-14 21:03:38 +0000

    Se corrige el archivo reciproco.c

--- src/reciproco.c
+++ src/reciproco.c
@@ -1,15 +1,22 @@
 void
 multrecip (float n[10][11], int N, int i)
 {
-  float r = 0, d = 0;
+  float r = 1, d = 0;
   int j;
   for (j = 0; j < N; j++)
     {
-      if (n[i][j] != 1)
+      if (n[i][j] == 1)
 	{
-	  r = n[i][j];
-	  d = j;
 	  break;
+	}
+      else
+	{
+	  if (n[i][j] != 0)
+	    {
+		r = n[i][j];
+		d = j;
+		break;
+	     }
 	}
     }
   r = (1 / r);
