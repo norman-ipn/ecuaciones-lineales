2f3de58594b2aff7f1766fef4447238e6ea8fa2b
Ivan David Sanchez Espinosa <ivandavidse@hotmail.com>
2018-06-15 01:32:49 -0500

    Se corrigió la funcion pedirn.c y su header

--- src/pedirn.c
+++ src/pedirn.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
-#define TAM 100
-int datos (){ /* funcion para que pida tamaño de la matriz */
-    int i;
-
-    do{
-        printf("Introduzca el numero de variables que incluira la matriz (Introduzca un numero menor a 100):\n");
-        scanf("%i", &i);    /*introducimos el tamaño de la matriz*/
-        if(i > TAM && i <= 0){
-            printf("el tamaño no es valido, intente de nuevo.");
-        }else
-            break;
-    }while(1);  /*Si no es del tamaño adecuado, indicarlo*/
- 
-    return i;
+int
+pedirn ()
+{
+  int n;
+  do
+    {
+      printf ("\nNumero de ecuacions que desea ingresar: ");
+      scanf ("%d", &n);
+      if (n < 0)
+	{
+	  printf ("Ingresar un numero mayor a cero\n\n");
+	}
+      else if (n > 10)
+	{
+	  printf ("Ingresar un numero menor a 11\n\n");
+	}
+    }
+  while (n < 0 || n > 10);
+  printf ("\n");
+  return n;
 }
-
--- src/pedirn.h
+++ src/pedirn.h
@@ -1,2 +1,1 @@
-int datos();
-
+int pedirn ();
