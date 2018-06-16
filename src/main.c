69260e6dbdfda329595cc64e78ec9025ecd718ef
Ivan David Sanchez Espinosa <ivandavidse@hotmail.com>
2018-06-16 10:58:30 -0500

    Se corrigio error de referencia de pedirn

--- src/main.c
+++ src/main.c
@@ -1,34 +1,33 @@
 #include<stdio.h>
-#include"sustituir.h"
-#include"restar.h"
-#include"reciproco.h"
-#include"funcion_pedir_valores.h"
-#include"pedirn.h"
-#include"mostrar.h"
-#include"imprimematriz.h"
+#include "sustituir.h"
+#include "restar.h"
+#include "reciproco.h"
+#include "funcion_pedir_valores.h"
+#include "pedirn.h"
+#include "mostrar.h"
+#include "imprimematriz.h"
 
 int
 main (void)
 {
   float m[10][11];
-  float x[10];
+  float x[11];
   int N, i = 0, j = 0, g = 0;
-  printf ("\n---------------ELIMINACION DE GAUSS-------------\n");
+  printf ("\n--------------ELIMINACION DE GAUSS--------------\n");
   N = pedirn ();
   pedirvalores (m, N);
   printf ("La matriz ingresada es: \n");
   impmat (m, N);
   for (g = 0; g < (N + 1); g++)
     {
-
       for (i = g; i < N; i++)
-        {
-          multrecip (m, N, i);
-          for (j = i + 1; j < N; j++)
-            {
-              restar (m, i, j, g, N);
-            }
-        }
+	{
+	  multrecip (m, N, i);
+	  for (j = i + 1; j < N; j++)
+	    {
+	      restar (m, i, j, g, N);
+	    }
+	}
     }
   printf ("\nLa matriz final es: \n");
   sustituir (m, x, N);
@@ -38,4 +37,3 @@ main (void)
   printf ("\n");
   return 0;
 }
-
