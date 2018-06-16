30234be2b0e6820b0af398019969147e867b8e6a
Ivan David Sanchez Espinosa <ivandavidse@hotmail.com>
2018-06-16 12:39:30 -0500

    Se agregó el modulo recibirnumero.c al main.c oficial

--- raizcuadrada/main.c
+++ raizcuadrada/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include "encontrarn.h"
+#include "hacerpares.h"
+#include "recibirnumero.h"
 int
 main ()
 {
-
-
+  int r = 0, N = 0, par[5];
+  recibirnumero (r, N, par);
   return 0;
 }
