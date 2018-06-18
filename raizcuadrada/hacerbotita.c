2bffac9bcd5b90726965b138a9fda572f4469676
Ivan David Sanchez Espinosa <ivandavidse@hotmail.com>
2018-06-18 11:48:30 -0500

    Se resolvieron errores, sin embargo el programa solo funciona con raices de digitos pares.

--- raizcuadrada/hacerbotita.c
+++ raizcuadrada/hacerbotita.c
@@ -1,8 +1,10 @@
+#include<stdio.h>
 #include "concatenar.h"
 int
 hacerbotita (int par[5], int p, int resul)
 {
-  int i = 0, t = 0, tem = 0, digito = 0, h = 0, q = 0, k = 0, nx = 0;
+  int i = 0, t = 0, tem = 0, digito = 0, h = 0, q = 0, k = 0, nx = 0, found =
+    0;
   if (resul != 0)
     {
       for (p = p; p < 4; p++)
@@ -10,22 +12,46 @@ hacerbotita (int par[5], int p, int resul)
 	  t = par[p] - (resul * resul);
 	  h = t * 100 + par[p + 1];
 	  k = resul * 2;
-	  if (h > 99 && h <= 999)
+	  if (t == 0)
 	    {
-	      q = h / 10;
-	    }
-	  else if (h > 999 && h <= 9999)
-	    {
-	      q = h / 100;
-	    }
-	  for (i = 1; i <= 10; i++)
-	    {
-	      tem = i * k;
-	      if (tem > q)
+	      q = h;
+	      for (i = 1; i <= 10; i++)
 		{
-		  digito = i - 1;
-		  break;
+		  tem = concatenar (k, i);
+		  if (tem == q)
+		    {
+		      digito = i;
+		      break;
+		    }
+		  else if (tem > q)
+		    {
+		      digito = i - 1;
+		      break;
+		    }
 		}
+	    }
+	  else
+	    {
+	      if (h > 99 && h <= 999)
+		{
+		  q = h / 10;
+		}
+	      else if (h > 999 && h <= 9999)
+		{
+		  q = h / 100;
+		}
+	      i = 1;
+	      do
+		{
+		  i++;
+		  tem = i * k;
+		  if (tem > q)
+		    {
+		      digito = i - 1;
+		      found = 1;
+		    }
+		}
+	      while (found == 0);
 	    }
 	  nx = concatenar (k, digito) * digito;
 	  if (nx > h)
